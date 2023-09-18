d39154cbeeaa53eaaf1c694deaa35ef4e4cb6345
Waterpig <wtryoutube@seznam.cz>
2023-09-18 15:44:18 +0200

    More moff

--- tgui/packages/tgui/interfaces/ZubbersFlappyMoth.tsx
+++ tgui/packages/tgui/interfaces/ZubbersFlappyMoth.tsx
@@ -1,5 +1,6 @@
 import { useBackend } from '../backend';
 import { NtosWindow } from '../layouts';
+import { globalEvents } from '../events';
 
 // The player character
 type FlapMoth = {
@@ -21,6 +22,7 @@ type FlapProps = {};
 
 type FlapState = {
   player: FlapMoth;
+  score: number;
 };
 
 // FLAPPY MOTH minigame class
@@ -40,6 +42,63 @@ class FlapMinigame {
         hitbox: this.playerhitbox,
       },
     };
+
+    // Controls handling!!
+    this.handle_mousedown = this.handle_mousedown.bind(this);
+    this.handle_mouseup = this.handle_mouseup.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+    this.handle_ctrldown = this.handle_ctrldown.bind(this);
+    this.handle_ctrlup = this.handle_ctrlup.bind(this);
+
+    // Updates the animation
+    this.updateAnimation = this.updateAnimation.bind(this);
+
+    // Binds the moff movement
+    this.moveMoth = this.moveMoth.bind(this);
+  }
+  componentDidMount() {
+    // add binds blah blah
+    document.addEventListener('mousedown', this.handle_mousedown);
+    document.addEventListener('mouseup', this.handle_mouseup);
+    this.animation_id = window.requestAnimationFrame(this.updateAnimation);
+    globalEvents.on('byond/mousedown', this.handle_mousedown);
+    globalEvents.on('byond/mouseup', this.handle_mouseup);
+    globalEvents.on('byond/ctrldown', this.handle_ctrldown);
+    globalEvents.on('byond/ctrlup', this.handle_ctrlup);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handle_mousedown);
+    document.removeEventListener('mouseup', this.handle_mouseup);
+    window.cancelAnimationFrame(this.animation_id);
+    globalEvents.off('byond/mousedown', this.handle_mousedown);
+    globalEvents.off('byond/mouseup', this.handle_mouseup);
+    globalEvents.off('byond/ctrldown', this.handle_ctrldown);
+    globalEvents.off('byond/ctrlup', this.handle_ctrlup);
+  }
+
+  moveMoth(state: FlapState, delta: number): FlapState {
+    // Delta time when we lag
+    const seconds = delta / 1000;
+    // The moth
+    const { player } = this.state;
+    // Speedup when flapping
+    const acceleration_up = -1500;
+    // Oop here comes gravity
+    const acceleration_down = 1000;
+
+    let newPosition = player.position + seconds * player.velocity;
+    // let newVelocity = player.velocity; why
+
+    // bounding the position
+    if (newPosition < 0) {
+      newPosition = 0;
+      player.velocity = 0;
+    } else if (newPosition + player.hitbox > this.gameheight) {
+      newPosition = this.gameheight - player.hitbox;
+      player.velocity = 0;
+    }
   }
 }
 
