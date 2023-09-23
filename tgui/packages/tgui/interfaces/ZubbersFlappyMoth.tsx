0a56ecda58f16a38250107e83a61e0e0856f4bcb
Waterpig <wtryoutube@seznam.cz>
2023-09-23 15:20:09 +0200

    The screams of the damned

--- tgui/packages/tgui/interfaces/ZubbersFlappyMoth.tsx
+++ tgui/packages/tgui/interfaces/ZubbersFlappyMoth.tsx
@@ -1,6 +1,8 @@
 import { useBackend } from '../backend';
 import { NtosWindow } from '../layouts';
-import { globalEvents } from '../events';
+import { Component } from 'inferno';
+import { globalEvents, KeyEvent } from '../events';
+import { KEY_CTRL } from '../../common/keycodes';
 
 // The player character
 type FlapMoth = {
@@ -25,31 +27,37 @@ type FlapState = {
   score: number;
 };
 
+enum FlapMovementState {
+  Idle,
+  Flap,
+}
+
 // FLAPPY MOTH minigame class
-class FlapMinigame {
+class FlapMinigame extends Component<FlapMovementState, FlapProps> {
   animation_id: number;
   last_frame: number;
   state: FlapState;
+  movementstate: FlapMovementState;
   gameheight: number = 500;
 
   playerhitbox: number = 10;
 
-  constructor() {
+  constructor(props: FlapProps) {
+    super(props);
+
     this.state = {
       player: {
         position: this.gameheight / 2,
         velocity: 0,
         hitbox: this.playerhitbox,
       },
+      score: 0,
     };
 
     // Controls handling!!
     this.handle_mousedown = this.handle_mousedown.bind(this);
-    this.handle_mouseup = this.handle_mouseup.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.handleKeyUp = this.handleKeyUp.bind(this);
+    this.handle_keydown = this.handle_keydown.bind(this);
     this.handle_ctrldown = this.handle_ctrldown.bind(this);
-    this.handle_ctrlup = this.handle_ctrlup.bind(this);
 
     // Updates the animation
     this.updateAnimation = this.updateAnimation.bind(this);
@@ -57,25 +65,21 @@ class FlapMinigame {
     // Binds the moff movement
     this.moveMoth = this.moveMoth.bind(this);
   }
+
+  // add binds
   componentDidMount() {
-    // add binds blah blah
     document.addEventListener('mousedown', this.handle_mousedown);
-    document.addEventListener('mouseup', this.handle_mouseup);
     this.animation_id = window.requestAnimationFrame(this.updateAnimation);
     globalEvents.on('byond/mousedown', this.handle_mousedown);
-    globalEvents.on('byond/mouseup', this.handle_mouseup);
     globalEvents.on('byond/ctrldown', this.handle_ctrldown);
-    globalEvents.on('byond/ctrlup', this.handle_ctrlup);
   }
 
+  // remove binds
   componentWillUnmount() {
     document.removeEventListener('mousedown', this.handle_mousedown);
-    document.removeEventListener('mouseup', this.handle_mouseup);
     window.cancelAnimationFrame(this.animation_id);
     globalEvents.off('byond/mousedown', this.handle_mousedown);
-    globalEvents.off('byond/mouseup', this.handle_mouseup);
     globalEvents.off('byond/ctrldown', this.handle_ctrldown);
-    globalEvents.off('byond/ctrlup', this.handle_ctrlup);
   }
 
   moveMoth(state: FlapState, delta: number): FlapState {
@@ -89,15 +93,44 @@ class FlapMinigame {
     const acceleration_down = 1000;
 
     let newPosition = player.position + seconds * player.velocity;
-    // let newVelocity = player.velocity; why
+
+    let newVelocity = player.velocity;
 
     // Bounding the position so we don't flap our way out
     if (newPosition < 0) {
       newPosition = 0;
-      player.velocity = 0;
+      newVelocity = 0;
     } else if (newPosition + player.hitbox > this.gameheight) {
       newPosition = this.gameheight - player.hitbox;
-      player.velocity = 0;
+      newVelocity = 0;
+    }
+
+    if (Math.abs(player.velocity) < 0.01) {
+      newVelocity = 0;
+    }
+
+    const newState: FlapState = {
+      ...state,
+      player: { ...player, position: newPosition, velocity: newVelocity },
+    };
+    return newState;
+  }
+
+  handle_mousedown(event: MouseEvent) {
+    if (this.movementstate === FlapMovementState.Idle) {
+      this.movementstate = FlapMovementState.Flap;
+    }
+  }
+
+  handle_keydown(keyEvent: KeyEvent) {
+    if (keyEvent.code === KEY_CTRL) {
+      this.handle_ctrldown;
+    }
+  }
+
+  handle_ctrldown() {
+    if (this.movementstate === FlapMovementState.Idle) {
+      this.movementstate = FlapMovementState.Flap;
     }
   }
 }
