97d009bcbcd0e95924cae98f21719f97fa84c522
StrangeWeirdKitten <95130227+StrangeWeirdKitten@users.noreply.github.com>
2023-11-08 21:49:41 -0700

    hypno rewrite

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/bubber/hypno_status.tsx
@@ -0,0 +1,6 @@
+import { FeatureDropdownInput, FeatureChoiced } from '../../base';
+
+export const erp_status_pref_hypno: FeatureChoiced = {
+  name: 'ERP Hypnosis Status',
+  component: FeatureDropdownInput,
+};
