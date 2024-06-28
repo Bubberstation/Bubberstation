Bubberstation/Bubberstation

commit 16f66806de8f13bd1fce33f909616da7890fc05c

F3-LINE cyborg updates attempt 2 [Modularish] (#1641)
## About The Pull Request

This attempt two of
https://github.com/Bubberstation/Bubberstation/pull/1636

Makes the sprite file resolutions from 64 x 32 to 32 x 32 for cat borgs

Makes 32 x 32 quadruped cybrogs into it's own borg type with it's own
quadruped module of it's own.

Allows only station standard cat borgs to be hold able, non standard can
not be held.

Adds code for cyborgs to indicate when they are being held.

Makes cyborg that are being held unable to interact items around them,
they can still break free any time.

This does not allow cyborgs with shape shifting to be picked up it only
allows the REAL model to be held.

A slight change from the last PR is the addition of a cargo variant of
cat borg.

<!-- Describe The Pull Request. Please be sure every change is
documented or this can delay review and even discourage maintainers from
merging your PR! -->

<!-- Please make sure to actually test your PRs. If you have not tested
your PR mention it. -->

## Why It's Good For The Game

<!-- Argue for the merits of your changes and how they benefit the game,
especially if they are controversial and/or far reaching. If you can't
actually explain WHY what you are doing will improve the game, then it
probably isn't good for the game in the first place. -->

I've always wondered why can't I make cat borgs hold able and also make
it so it don't have sweeping mechanical changes to the game so I came up
with a way to allow them to be held disabling their abilities to
interact with much wile being in a held state and being almost
unbaggable.

Cargo cat borg was added since there was no variant.

Making the sprites 32 x 32 means they won't look so offsetted when using
things quite as often.

Cent Com don’t want you to know this but the the cat borgs on space
station 13 are free you can take them home I have 458 cat borgs at home.

## Proof Of Testing

code/modules/mob/living/silicon/robot/robot_model.dm
@@ -250,6 +250,10 @@
 	new_model.update_dogborg()
 	new_model.update_tallborg()
 	//SKYRAT EDIT ADDITION END
+	//BUBBER EDIT ADDTION BEGIN
+	new_model.update_squadruped()
+	new_model.update_lightweight()
+	//BUBBER EDIT ADDTION END
 
 	INVOKE_ASYNC(new_model, PROC_REF(do_transform_animation))
 	qdel(src)

modular_skyrat/modules/borgs/code/robot_items.dm
@@ -731,6 +731,7 @@
 	user.update_icons()
 	user.model.update_dogborg()
 	user.model.update_tallborg()
+	user.model.update_squadruped() //BUBBER ADDITION
 
 	if(listeningTo == user)
 		return

modular_zubbers/code/modules/borgs/code/robot_model.dm
@@ -1,4 +1,34 @@
 // The actual code to work these in
+
+// Cyborg model types
+
+//For small quadruped 32 X 32 cyborgs suchs as F3-LINE cyborgs
+/obj/item/robot_model/proc/update_squadruped()
+	var/mob/living/silicon/robot/cyborg = robot || loc
+	if (!istype(robot))
+		return
+	if (model_features && (TRAIT_R_SQUADRUPED in model_features))
+		hat_offset = INFINITY
+		add_verb(cyborg, /mob/living/silicon/robot/proc/robot_lay_down)
+		add_verb(cyborg, /mob/living/silicon/robot/proc/rest_style)
+	else
+		remove_verb(cyborg, /mob/living/silicon/robot/proc/robot_lay_down)
+		remove_verb(cyborg, /mob/living/silicon/robot/proc/rest_style)
+
+//For cyborgs who have a lighter chassis
+// NOTE WORKS BEST WITH ONLY 32 X 32 CYBORBG SPRITES!!!
+/obj/item/robot_model/proc/update_lightweight()
+	var/mob/living/silicon/robot/cyborg = robot || loc
+	if (!istype(robot))
+		return
+	if (model_features && (TRAIT_R_LIGHT_WEIGHT in model_features))
+		cyborg.can_be_held = TRUE
+		cyborg.held_w_class = WEIGHT_CLASS_HUGE
+	else
+		cyborg.can_be_held = FALSE
+		cyborg.held_w_class = WEIGHT_CLASS_NORMAL
+
+// Centcom cyborgs
 /obj/item/robot_model/centcom
 	name = "Central Command"
 	basic_modules = list(
@@ -60,7 +90,6 @@
 	cyborg.req_access = list(ACCESS_ROBOTICS)
 	cyborg.faction -= ROLE_DEATHSQUAD //You're no longer part of CENTCOM
 
-
 //Research cyborgs
 /obj/item/robot_model/sci
 	name = "Research"
@@ -105,7 +134,7 @@
 		"F3-LINE" = list(
 		SKIN_ICON_STATE = CYBORG_ICON_TYPE_SCI_CATBORG,
 		SKIN_ICON = CYBORG_ICON_ALL_CATBORG,
-		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_WIDE, TRAIT_R_SMALL)
+		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SQUADRUPED, TRAIT_R_SMALL, TRAIT_R_LIGHT_WEIGHT)
 		),
 		"Vale" = list(
 			SKIN_ICON_STATE = "vale",
@@ -192,7 +221,7 @@
 		"F3-LINE" = list(
 		SKIN_ICON_STATE = CYBORG_ICON_TYPE_GEN_CATBORG,
 		SKIN_ICON = CYBORG_ICON_ALL_CATBORG,
-		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_WIDE, TRAIT_R_SMALL)
+		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SQUADRUPED, TRAIT_R_SMALL, TRAIT_R_LIGHT_WEIGHT)
 		),
 		"SmolRaptor" = list(SKIN_ICON_STATE = CYBORG_ICON_TYPE_SMOLRAPTOR,
 		SKIN_ICON = CYBORG_ICON_GEN_SMOLRAPTOR,
@@ -206,7 +235,7 @@
 		"F3-LINE" = list(
 		SKIN_ICON_STATE = CYBORG_ICON_TYPE_MED_CATBORG,
 		SKIN_ICON = CYBORG_ICON_ALL_CATBORG,
-		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_WIDE, TRAIT_R_SMALL)
+		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SQUADRUPED, TRAIT_R_SMALL, TRAIT_R_LIGHT_WEIGHT)
 		),
 		"Raptor" = list(
 			SKIN_ICON_STATE = CYBORG_ICON_TYPE_RAPTOR,
@@ -225,7 +254,7 @@
 		"F3-LINE" = list(
 		SKIN_ICON_STATE = CYBORG_ICON_TYPE_ENG_CATBORG,
 		SKIN_ICON = CYBORG_ICON_ALL_CATBORG,
-		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_WIDE, TRAIT_R_SMALL)
+		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SQUADRUPED, TRAIT_R_SMALL, TRAIT_R_LIGHT_WEIGHT)
 		),
 		"Raptor" = list(
 			SKIN_ICON_STATE = CYBORG_ICON_TYPE_RAPTOR,
@@ -244,7 +273,7 @@
 		"F3-LINE" = list(
 		SKIN_ICON_STATE = CYBORG_ICON_TYPE_JANI_CATBORG,
 		SKIN_ICON = CYBORG_ICON_ALL_CATBORG,
-		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_WIDE, TRAIT_R_SMALL)
+		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SQUADRUPED, TRAIT_R_SMALL, TRAIT_R_LIGHT_WEIGHT)
 		),
 		//64x48 sprites below (Raptor)
 		"Raptor" = list(
@@ -264,7 +293,7 @@
 		"F3-LINE" = list(
 		SKIN_ICON_STATE = CYBORG_ICON_TYPE_MINE_CATBORG,
 		SKIN_ICON = CYBORG_ICON_ALL_CATBORG,
-		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_WIDE, TRAIT_R_SMALL)
+		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SQUADRUPED, TRAIT_R_SMALL, TRAIT_R_LIGHT_WEIGHT)
 		),
 		//64x32 Sprites below (Wide)
 		"Corrupt" = list(
@@ -295,7 +324,7 @@
 		"F3-LINE" = list(
 		SKIN_ICON_STATE = CYBORG_ICON_TYPE_SEC_CATBORG,
 		SKIN_ICON = CYBORG_ICON_ALL_CATBORG,
-		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_WIDE, TRAIT_R_SMALL)
+		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SQUADRUPED, TRAIT_R_SMALL, TRAIT_R_LIGHT_WEIGHT)
 		),
 		//32x64 Sprites below (Tall)
 		"Meka - Bluesec" = list(
@@ -318,7 +347,7 @@
 		"F3-LINE" = list(
 		SKIN_ICON_STATE = CYBORG_ICON_TYPE_PK_CATBORG,
 		SKIN_ICON = CYBORG_ICON_ALL_CATBORG,
-		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_WIDE, TRAIT_R_SMALL)
+		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SQUADRUPED, TRAIT_R_SMALL, TRAIT_R_LIGHT_WEIGHT)
 		),
 		//32x64 Sprites below (Tall)
 		"Meka - Bluesec" = list(
@@ -352,7 +381,7 @@
 		"F3-LINE" = list(
 		SKIN_ICON_STATE = CYBORG_ICON_TYPE_SERV_CATBORG,
 		SKIN_ICON = CYBORG_ICON_ALL_CATBORG,
-		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_WIDE, TRAIT_R_SMALL)
+		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SQUADRUPED, TRAIT_R_SMALL, TRAIT_R_LIGHT_WEIGHT)
 		),
 		"Fancy Raptor" = list(
 			SKIN_ICON_STATE = "fancyraptor",
@@ -371,6 +400,11 @@
 		"SmolRaptor" = list(SKIN_ICON_STATE = CYBORG_ICON_TYPE_SMOLRAPTOR,
 		SKIN_ICON = CYBORG_ICON_CAR_SMOLRAPTOR,
 		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SMALL, TRAIT_R_WIDE)
+		),
+		"F3-LINE" = list(
+		SKIN_ICON_STATE = CYBORG_ICON_TYPE_CAR_CATBORG,
+		SKIN_ICON = CYBORG_ICON_ALL_CATBORG,
+		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SQUADRUPED, TRAIT_R_SMALL, TRAIT_R_LIGHT_WEIGHT)
 		),
 	)
 
@@ -405,7 +439,7 @@
 		"F3-LINE" = list(
 		SKIN_ICON_STATE = CYBORG_ICON_TYPE_SYNDI_CATBORG,
 		SKIN_ICON = CYBORG_ICON_ALL_CATBORG,
-		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_WIDE, TRAIT_R_SMALL)
+		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SQUADRUPED, TRAIT_R_SMALL)
 		),
 
 	)
@@ -430,7 +464,7 @@
 		"F3-LINE" = list(
 		SKIN_ICON_STATE = CYBORG_ICON_TYPE_SYNDI_CATBORG,
 		SKIN_ICON = CYBORG_ICON_ALL_CATBORG,
-		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_WIDE, TRAIT_R_SMALL)
+		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SQUADRUPED, TRAIT_R_SMALL)
 		),
 	)
 
@@ -456,7 +490,7 @@
 		"F3-LINE" = list(
 		SKIN_ICON_STATE = CYBORG_ICON_TYPE_SYNDI_CATBORG,
 		SKIN_ICON = CYBORG_ICON_ALL_CATBORG,
-		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_WIDE, TRAIT_R_SMALL)
+		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SQUADRUPED, TRAIT_R_SMALL)
 		),
 	)
 
@@ -476,7 +510,7 @@
 		"F3-LINE" = list(
 		SKIN_ICON_STATE = CYBORG_ICON_TYPE_SYNDI_CATBORG,
 		SKIN_ICON = CYBORG_ICON_ALL_CATBORG,
-		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_WIDE, TRAIT_R_SMALL)
+		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SQUADRUPED, TRAIT_R_SMALL)
 		),
 	)
 
@@ -499,7 +533,7 @@
 		"F3-LINE" = list(
 		SKIN_ICON_STATE = CYBORG_ICON_TYPE_NINJA_CATBORG,
 		SKIN_ICON = CYBORG_ICON_ALL_CATBORG,
-		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_WIDE, TRAIT_R_SMALL)
+		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SQUADRUPED, TRAIT_R_SMALL)
 		),
 	)
 
@@ -509,7 +543,7 @@
 		"F3-LINE" = list(
 		SKIN_ICON_STATE = CYBORG_ICON_TYPE_NINJA_CATBORG,
 		SKIN_ICON = CYBORG_ICON_ALL_CATBORG,
-		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_WIDE, TRAIT_R_SMALL)
+		SKIN_FEATURES = list(TRAIT_R_UNIQUEWRECK, TRAIT_R_SQUADRUPED, TRAIT_R_SMALL)
 		),
 	)
 

code/__DEFINES/~~bubber_defines/traits.dm
@@ -6,3 +6,9 @@
 //Cyborg traits
 
 #define TRAIT_RESEARCH_CYBORG "research_cyborg"
+
+/// Cyborgs with unique sprites
+/// 32x32 quadruped skins with resting, sitting, and belly up sprites
+#define TRAIT_R_SQUADRUPED "squadruped_borg"
+// For light weight borgs
+#define TRAIT_R_LIGHT_WEIGHT "lightweight_borg"

code/_globalvars/traits/_traits.dm
@@ -678,6 +678,8 @@ GLOBAL_LIST_INIT(traits_by_type, list(
 		"TRAIT_R_TALL" = TRAIT_R_TALL,
 		"TRAIT_R_SMALL" = TRAIT_R_SMALL,
 		"TRAIT_R_UNIQUEPANEL" = TRAIT_R_UNIQUEPANEL,
+		"TRAIT_R_SQUADRUPED" = TRAIT_R_SQUADRUPED, //BUBBER addition - for small quadruped 32x32 cyborgs
+		"TRAIT_R_LIGHT_WEIGHT" = TRAIT_R_LIGHT_WEIGHT, //BUBBER addition for small light weighted cyborgs
 		"TRAIT_REAGENT" = TRAIT_REAGENT,
 		"TRAIT_REVIVES_BY_HEALING" = TRAIT_REVIVES_BY_HEALING,
 		"TRAIT_RIGGER" = TRAIT_RIGGER,

code/_globalvars/traits/admin_tooling.dm
@@ -332,6 +332,8 @@ GLOBAL_LIST_INIT(admin_visible_traits, list(
 		"TRAIT_R_TALL" = TRAIT_R_TALL,
 		"TRAIT_R_SMALL" = TRAIT_R_SMALL,
 		"TRAIT_R_UNIQUEPANEL" = TRAIT_R_UNIQUEPANEL,
+		"TRAIT_R_SQUADRUPED" = TRAIT_R_SQUADRUPED, //BUBBER addition - for small quadruped 32x32 cyborgs
+		"TRAIT_R_LIGHT_WEIGHT" = TRAIT_R_LIGHT_WEIGHT, //BUBBER addition for small light weighted cyborgs
 		"TRAIT_REAGENT" = TRAIT_REAGENT,
 		"TRAIT_REVIVES_BY_HEALING" = TRAIT_REVIVES_BY_HEALING,
 		"TRAIT_RIGGER" = TRAIT_RIGGER,

code/_onclick/cyborg.dm
@@ -14,7 +14,7 @@
 	if(check_click_intercept(params,A))
 		return
 
-	if(stat || (lockcharge) || IsParalyzed() || IsStun())
+	if(stat || (lockcharge) || IsParalyzed() || IsStun() || istype(loc, /obj/item/clothing/head/mob_holder)) //BUBBER EDIT - makes it so cyborgs that are holdable can not interact wile being held
 		return
 
 	var/list/modifiers = params2list(params)

code/modules/mob/living/silicon/robot/robot.dm
@@ -477,7 +477,8 @@
  */
 /mob/living/silicon/robot/proc/toggle_headlamp(turn_off = FALSE, update_color = FALSE)
 	//if both lamp is enabled AND the update_color flag is on, keep the lamp on. Otherwise, if anything listed is true, disable the lamp.
-	if(!(update_color && lamp_enabled) && (turn_off || lamp_enabled || update_color || !lamp_functional || stat || low_power_mode))
+	//BUBBER EDIT - Disables flashlight when held
+	if(!(update_color && lamp_enabled) && (turn_off || lamp_enabled || update_color || !lamp_functional || stat || low_power_mode || istype(loc, /obj/item/clothing/head/mob_holder)))
 		set_light_on(lamp_functional && stat != DEAD && lamp_doom) //If the lamp isn't broken and borg isn't dead, doomsday borgs cannot disable their light fully.
 		set_light_color(COLOR_RED) //This should only matter for doomsday borgs, as any other time the lamp will be off and the color not seen
 		set_light_range(1) //Again, like above, this only takes effect when the light is forced on by doomsday mode.

commit 97c8e481874982c7ffc848a1e968540f607d31c2

Bug fix / refactor-ish to cyborg resting procs (#1657)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->
<!-- You can view Contributing.MD for a detailed description of the pull
request process. -->

## About The Pull Request

Fixes bug caused by
https://github.com/Bubberstation/Bubberstation/pull/1641 not allowing
non small quad cyborgs to change rest states

Adds missing verbs for some cyborgs back.

<!-- Describe The Pull Request. Please be sure every change is
documented or this can delay review and even discourage maintainers from
merging your PR! -->

<!-- Please make sure to actually test your PRs. If you have not tested
your PR mention it. -->

## Why It's Good For The Game

<!-- Argue for the merits of your changes and how they benefit the game,
especially if they are controversial and/or far reaching. If you can't
actually explain WHY what you are doing will improve the game, then it
probably isn't good for the game in the first place. -->

It fixes a bug I caused sorrwwyy.

## Proof Of Testing

<!-- Compile and run your code locally. Make sure it works. This is the
place to show off your changes! We are not responsible for testing your
features. -->

Not sure how to show it working other than saying I compiled it gave the
missing verbs back that were gone.

## Changelog

<!-- If your PR modifies aspects of the game that can be concretely
observed by players or admins you should add a changelog. If your change
does NOT meet this description, remove this section. Be sure to properly
mark your PRs to prevent unnecessary GBP loss. You can read up on GBP
and it's effects on PRs in the tgstation guides for contributors. Please
note that maintainers freely reserve the right to remove and add tags
should they deem it appropriate. You can attempt to finagle the system
all you want, but it's best to shoot for clear communication right off
the bat. -->

:cl:

code/modules/mob/living/silicon/robot/robot_model.dm
@@ -247,12 +247,13 @@
 	log_silicon("CYBORG: [key_name(cyborg)] has transformed into the [new_model] model.")
 
 	//SKYRAT EDIT ADDITION BEGIN - ALTBORGS - Old check for 'dogborg' var no longer necessary, refactored into model_features instead.
-	new_model.update_dogborg()
+	//new_model.update_dogborg() // BUBBER REMOVAL
 	new_model.update_tallborg()
 	//SKYRAT EDIT ADDITION END
 	//BUBBER EDIT ADDTION BEGIN
-	new_model.update_squadruped()
+	new_model.update_quadruped()
 	new_model.update_lightweight()
+	new_model.update_robot_rest()
 	//BUBBER EDIT ADDTION END
 
 	INVOKE_ASYNC(new_model, PROC_REF(do_transform_animation))

modular_skyrat/modules/borgs/code/robot_items.dm
@@ -729,9 +729,12 @@
 	user.bubble_icon = "robot"
 	active = TRUE
 	user.update_icons()
-	user.model.update_dogborg()
+	//user.model.update_dogborg() //BUBBER REMOVAL
 	user.model.update_tallborg()
-	user.model.update_squadruped() //BUBBER ADDITION
+	//BUBBER EDIT ADDTION BEGIN
+	user.model.update_quadruped()
+	user.model.update_robot_rest()
+	//BUBBER EDIT ADDTION END
 
 	if(listeningTo == user)
 		return
@@ -755,8 +758,12 @@
 	user.bubble_icon = saved_bubble_icon
 	active = FALSE
 	user.update_icons()
-	user.model.update_dogborg()
+	//user.model.update_dogborg() //BUBBER REMOVAL
 	user.model.update_tallborg()
+	//BUBBER EDIT ADDTION BEGIN
+	user.model.update_quadruped()
+	user.model.update_robot_rest()
+	//BUBBER EDIT ADDTION END
 
 /obj/item/borg_shapeshifter/proc/disrupt(mob/living/silicon/robot/user)
 	SIGNAL_HANDLER

modular_skyrat/modules/borgs/code/robot_model.dm
@@ -14,7 +14,7 @@
 	if (model_features && (TRAIT_R_TALL in model_features))
 		cyborg.maptext_height = 48 //Runechat blabla
 		cyborg.AddElement(/datum/element/footstep, FOOTSTEP_MOB_SHOE, 2, -6, sound_vary = TRUE)
-		add_verb(cyborg, /mob/living/silicon/robot/proc/robot_lay_down)
+		//add_verb(cyborg, /mob/living/silicon/robot/proc/robot_lay_down)// BUBBER REMOVAL - Moved into update_robot_rest()
 		switch(cyborg_base_icon)
 			if("mekamine")
 				cyborg.AddComponent(/datum/component/robot_smoke)
@@ -23,12 +23,13 @@
 	else
 		cyborg.maptext_height = initial(cyborg.maptext_height)
 		cyborg.RemoveElement(/datum/element/footstep, FOOTSTEP_MOB_SHOE, 2, -6, sound_vary = TRUE)
-		remove_verb(cyborg, /mob/living/silicon/robot/proc/robot_lay_down)
+		//remove_verb(cyborg, /mob/living/silicon/robot/proc/robot_lay_down)// BUBBER REMOVAL - Moved into update_robot_rest()
 		if(cyborg.GetComponent(/datum/component/robot_smoke))
 			qdel(cyborg.GetComponent(/datum/component/robot_smoke))
 			QDEL_NULL(cyborg.particles)	// Removing left over particles
 
-
+//BUBBER REMOVAL BEGIN - replaced with update_quadruped()
+/*
 /obj/item/robot_model/proc/update_dogborg()
 	var/mob/living/silicon/robot/cyborg = robot || loc
 	if (!istype(robot))
@@ -42,6 +43,8 @@
 		cyborg.set_base_pixel_x(0)
 		remove_verb(cyborg, /mob/living/silicon/robot/proc/robot_lay_down)
 		remove_verb(cyborg, /mob/living/silicon/robot/proc/rest_style)
+*/
+//BUBBER REMOVAL END
 
 //STANDARD
 /obj/item/robot_model/standard

modular_zubbers/code/modules/borgs/code/robot_model.dm
@@ -1,18 +1,20 @@
 // The actual code to work these in
 
-// Cyborg model types
+// Cyborg model trait procs below
 
-//For small quadruped 32 X 32 cyborgs suchs as F3-LINE cyborgs
-/obj/item/robot_model/proc/update_squadruped()
+//For all quadruped cyborgs
+/obj/item/robot_model/proc/update_quadruped()
 	var/mob/living/silicon/robot/cyborg = robot || loc
 	if (!istype(robot))
 		return
-	if (model_features && (TRAIT_R_SQUADRUPED in model_features))
+	if (model_features && ((TRAIT_R_SQUADRUPED in model_features) || (TRAIT_R_WIDE in model_features)))
 		hat_offset = INFINITY
-		add_verb(cyborg, /mob/living/silicon/robot/proc/robot_lay_down)
+		if (model_features && (TRAIT_R_WIDE in model_features))
+			cyborg.set_base_pixel_x(-16)
 		add_verb(cyborg, /mob/living/silicon/robot/proc/rest_style)
 	else
-		remove_verb(cyborg, /mob/living/silicon/robot/proc/robot_lay_down)
+		if (model_features && !(TRAIT_R_WIDE in model_features))
+			cyborg.set_base_pixel_x(0)
 		remove_verb(cyborg, /mob/living/silicon/robot/proc/rest_style)
 
 //For cyborgs who have a lighter chassis
@@ -27,6 +29,19 @@
 	else
 		cyborg.can_be_held = FALSE
 		cyborg.held_w_class = WEIGHT_CLASS_NORMAL
+
+//For cyborgs that can rest
+// Must have a resting state!
+/obj/item/robot_model/proc/update_robot_rest()
+	var/mob/living/silicon/robot/cyborg = robot || loc
+	if (!istype(robot))
+		return
+	if (model_features && ((TRAIT_R_SQUADRUPED in model_features) || (TRAIT_R_WIDE in model_features) || (TRAIT_R_TALL in model_features)))
+		add_verb(cyborg, /mob/living/silicon/robot/proc/robot_lay_down)
+	else
+		remove_verb(cyborg, /mob/living/silicon/robot/proc/robot_lay_down)
+
+// Cyborg model types below
 
 // Centcom cyborgs
 /obj/item/robot_model/centcom