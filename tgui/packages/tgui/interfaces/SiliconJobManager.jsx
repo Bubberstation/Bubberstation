6a6a82a9bc32d43a738b2c339249aa3f592f2947
Odairu <39929315+Odairu@users.noreply.github.com>
2024-08-10 17:28:36 -0400

    the whole PR

---
+++ tgui/packages/tgui/interfaces/SiliconJobManager.jsx
@@ -0,0 +1,62 @@
+import { useBackend } from '../backend';
+import { Box, Button, Dimmer, NoticeBox, Section, Table } from '../components';
+import { NtosWindow } from '../layouts';
+
+export const SiliconJobManager = (props) => {
+  return (
+    <NtosWindow width={400} height={620}>
+      <NtosWindow.Content scrollable>
+        <SiliconManagerContent />
+      </NtosWindow.Content>
+    </NtosWindow>
+  );
+};
+
+export const SiliconManagerContent = (props) => {
+  const { act, data } = useBackend();
+  const { authed, cooldown, slots = [], prioritized = [] } = data;
+  if (!authed) {
+    return (
+      <NoticeBox>
+        Current ID does not have access permissions to change job slots.
+      </NoticeBox>
+    );
+  }
+  return (
+    <Section>
+      {cooldown > 0 && (
+        <Dimmer>
+          <Box bold textAlign="center" fontSize="20px">
+            On Cooldown: {cooldown}s
+          </Box>
+        </Dimmer>
+      )}
+      <Table>
+        <Table.Row header>
+          <Table.Cell>Prioritized</Table.Cell>
+          <Table.Cell>Slots</Table.Cell>
+        </Table.Row>
+        {slots.map((slot) => (
+          <Table.Row key={slot.title} className="candystripe">
+            <Table.Cell bold>
+              <Button.Checkbox
+                fluid
+                content={slot.title}
+                disabled={slot.total <= 0}
+                checked={slot.total > 0 && prioritized.includes(slot.title)}
+                onClick={() =>
+                  act('Silicon_priority', {
+                    target: slot.title,
+                  })
+                }
+              />
+            </Table.Cell>
+            <Table.Cell collapsing>
+              {slot.current} / {slot.total}
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table>
+    </Section>
+  );
+};
