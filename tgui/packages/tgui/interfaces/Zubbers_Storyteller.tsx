a0fddbd46a2d6ceedd108213a5da052d58858a29
Waterpig <wtryoutube@seznam.cz>
2024-09-25 11:38:01 +0200

    even more track ui

--- tgui/packages/tgui/interfaces/Zubbers_Storyteller.tsx
+++ tgui/packages/tgui/interfaces/Zubbers_Storyteller.tsx
@@ -9,6 +9,7 @@ import {
   Stack,
 } from '../components';
 import { Window } from '../layouts';
+import { useState } from 'react';
 
 export type Storyteller_Data = {
   storyteller_name: String;
@@ -66,16 +67,23 @@ export const Zubbers_Storyteller_Round_Data = (props) => {
         <LabeledList.Item label="Storyteller status">
           <Button
             color={storyteller_halt ? 'red' : 'green'}
+            tooltip={(storyteller_halt ? 'Unhalt' : 'Halt') + ' storyteller'}
             onClick={() => act('halt_storyteller')}
+            width="20%"
+            textAlign="center"
           >
             {storyteller_halt ? 'Halted' : 'Running'}
           </Button>
         </LabeledList.Item>
-        <LabeledList.Item label="Players">
-          Active: {pop_data['active']}, Head: {pop_data['head']}, Sec:{' '}
+        <LabeledList.Item label="Active Players">
+          {pop_data['active']} {'('}Head: {pop_data['head']}, Sec:{' '}
           {pop_data['sec']}, Eng: {pop_data['eng']}, Med: {pop_data['med']}
+          {')'}
         </LabeledList.Item>
-        <LabeledList.Item label="Antag Cap">
+        <LabeledList.Item
+          label="Antag Cap"
+          tooltip="Amount of antags / The antag cap"
+        >
           <ProgressBar
             value={antag_count}
             maxValue={antag_cap}
@@ -92,19 +100,45 @@ export const Zubbers_Storyteller_Round_Data = (props) => {
   );
 };
 
+const TRACK_DATA_TRACK_WIDTH = '15%';
+const TRACK_DATA_POINT_WIDTH = '33%';
+const TRACK_DATA_NEXT_WIDTH = '17%';
+const TRACK_DATA_FORCED_WIDTH = '35%';
+
 export const Zubbers_Storyteller_Track_Data = (props) => {
   const { act, data } = useBackend<Storyteller_Data>();
-  const { tracks_data } = data;
+  const { tracks_data, storyteller_halt } = data;
   return (
     <Section title="Tracks">
-      <LabeledList>
+      <Stack vertical>
+        <Stack.Item>
+          <Stack bold>
+            <Stack.Item width={TRACK_DATA_TRACK_WIDTH}>Track</Stack.Item>
+            <Stack.Item width={TRACK_DATA_POINT_WIDTH}>Points</Stack.Item>
+            <Stack.Item width={TRACK_DATA_NEXT_WIDTH}>Next event</Stack.Item>
+            <Stack.Item width={TRACK_DATA_FORCED_WIDTH}>
+              Next forced event
+            </Stack.Item>
+          </Stack>
+        </Stack.Item>
+        <Stack.Divider></Stack.Divider>
         {Object.entries(tracks_data).map(([track, track_data]) => {
           const max_points = Number(track_data['max']);
           const current_points = Number(track_data['current']);
+          const forced = track_data['forced'];
           return (
-            <LabeledList.Item label={track}>
+            <Stack.Item>
               <Stack>
-                <Stack.Item width="33%">
+                <Stack.Item width={TRACK_DATA_TRACK_WIDTH}>
+                  <Button
+                    tooltip="Edit track parameters"
+                    width="100%"
+                    textAlign="center"
+                  >
+                    {track}
+                  </Button>
+                </Stack.Item>
+                <Stack.Item width={TRACK_DATA_POINT_WIDTH}>
                   <ProgressBar
                     value={current_points}
                     maxValue={max_points}
@@ -113,21 +147,34 @@ export const Zubbers_Storyteller_Track_Data = (props) => {
                       average: [max_points, Infinity],
                     }}
                   >
-                    {track_data['current']}
-                    {' / '}
-                    {track_data['max']}
+                    {current_points + ' / ' + max_points}
+                    {' (' +
+                      Math.floor((current_points * 100) / max_points) +
+                      '%) '}
                   </ProgressBar>
                 </Stack.Item>
-                <Stack.Item>
-                  <LabeledList.Item label="Next event">
-                    {track_data['next']} min
-                  </LabeledList.Item>
+                <Stack.Item width={TRACK_DATA_NEXT_WIDTH} textAlign="center">
+                  {storyteller_halt ? 'N/A' : '~' + track_data['next'] + 'min'}
+                </Stack.Item>
+                <Stack.Item width={TRACK_DATA_FORCED_WIDTH}>
+                  {forced ? forced : ''}
                 </Stack.Item>
               </Stack>
-            </LabeledList.Item>
+            </Stack.Item>
           );
         })}
-      </LabeledList>
+      </Stack>
+    </Section>
+  );
+};
+
+export const Zubbers_Storyteller_Scheduled_Data = (props) => {
+  const { act, data } = useBackend<Storyteller_Data>();
+  return (
+    <Section title="Scheduled events">
+      <Stack vertical>
+        <Stack.Item></Stack.Item>
+      </Stack>
     </Section>
   );
 };
