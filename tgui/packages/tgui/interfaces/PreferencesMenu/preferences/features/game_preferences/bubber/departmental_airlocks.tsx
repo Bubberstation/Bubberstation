fbeb91eb55f50f01762051ce7ac4d2259dfc07d4
lessthanthree <83487515+lessthnthree@users.noreply.github.com>
2024-11-20 12:05:25 -0800

    tgui

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/bubber/departmental_airlocks.tsx
@@ -0,0 +1,8 @@
+import { CheckboxInput, FeatureToggle } from '../../base';
+
+export const departmental_airlock_sounds: FeatureToggle = {
+  name: 'Departmental Airlock Sounds',
+  category: 'SOUND',
+  description: 'Enables fancy department-specific airlock sound effects.',
+  component: CheckboxInput,
+};
