10a3aa82fbe97402dad71908214e0d1f7f07c841
Swiftfeather <jackwars4@gmail.com>
2024-11-25 15:17:25 -0600

    updates outdated tooltip

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/bubber/antag_optin.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/bubber/antag_optin.tsx
@@ -5,7 +5,7 @@ import { FeatureDropdownInput } from '../../dropdowns';
 export const rr_opt_in_status_pref: FeatureChoiced = {
   name: 'Be Round Removed',
   description:
-    'Enabling any non-ghost antags \
-  (revenant, abductor contractor, etc.) will force your opt-in.',
+    'Displays to other players that you are \
+  are okay with being round removed.',
   component: FeatureDropdownInput,
 };
