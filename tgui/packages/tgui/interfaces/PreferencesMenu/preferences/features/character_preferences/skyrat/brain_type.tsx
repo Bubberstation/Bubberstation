018626e1317998de47d3790639c0537352a33926
ReturnToZender <110273561+ReturnToZender@users.noreply.github.com>
2024-12-06 05:03:41 -0600

    Minor character creator update

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/skyrat/brain_type.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/skyrat/brain_type.tsx
@@ -3,6 +3,6 @@ import { FeatureChoiced } from '../../base';
 import { FeatureDropdownInput } from '../../dropdowns';
 
 export const brain_type: FeatureChoiced = {
-  name: 'Silicon and Synthetic Brain Type',
+  name: 'Silicon, Synth Brain',
   component: FeatureDropdownInput,
 };
