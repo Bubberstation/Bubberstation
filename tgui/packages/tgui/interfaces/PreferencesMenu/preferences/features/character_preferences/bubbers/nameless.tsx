c83cf0261720bc39db0ee5afade62aea9126d828
The Sharkening <95130227+StrangeWeirdKitten@users.noreply.github.com>
2025-01-22 05:28:33 -0700

    nameless quirk

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/bubbers/nameless.tsx
@@ -0,0 +1,7 @@
+import { Feature, FeatureShortTextInput } from '../../base';
+
+export const nameless_quirk_name: Feature<string> = {
+  name: 'Prefix Name',
+  description: 'Example: (Prefix) #1334. Leave blank to default to job title.',
+  component: FeatureShortTextInput,
+};
