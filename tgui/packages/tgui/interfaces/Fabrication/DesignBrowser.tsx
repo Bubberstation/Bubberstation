a93a82a98e43db4e4cdf18cb7c7c3e7284eee63f
John Willard <53777086+JohnFulpWillard@users.noreply.github.com>
2025-02-05 13:02:16 -0500

    Fixes more scrollables for 516 (#89325)
    ## About The Pull Request
    
    I was looking at https://github.com/tgstation/tgstation/pull/89312
    (Ghommie merged it while I was looking at it lol, this would've been a
    comment otherwise) and noticed a few problems:
    
    1. There's other instances of ``style={{ overflow: 'auto' }}`` in the
    code, I checked them and they don't work, this fixed one instance of the
    3 that exists.
    2. Autolathes have 2 scrollbars, one for the whole UI and one for the
    item list (3 actually, one on the left for the sections)
    
    This fixes all instances, and uses overflowY instead of scrollable so it
    isn't there permanently.
    Also removes the unnecessary scrollbar to Autolathes.
    
    ## Why It's Good For The Game
    
    Fixes more UIs for 516 and removes unnecessary and annoying scrollbar
    from autolathes.
    
    ## Changelog
    
    :cl:
    fix: Autolathes no longer have a scrollbar that sends you off the
    screen.
    fix: Exosuit fabricators' queue list & Techfab/Autolathe's left side
    (with the sections of printables) now have a scrollbar when the full
    list doesn't fit on the UI for 516 users. Techfab/Autolathe also now
    does this for the list of items too, instead of always having one.
    /:cl:

--- tgui/packages/tgui/interfaces/Fabrication/DesignBrowser.tsx
+++ tgui/packages/tgui/interfaces/Fabrication/DesignBrowser.tsx
@@ -208,8 +208,8 @@ export const DesignBrowser = <T extends Design = Design>(
             <Stack.Item>
               <Section title="Categories" fitted />
             </Stack.Item>
-            <Stack.Item grow>
-              <Section fill style={{ overflow: 'auto' }}>
+            <Stack.Item grow style={{ overflowY: 'auto', overflowX: 'hidden' }}>
+              <Section fill>
                 <div className="FabricatorTabs">
                   <div
                     className={classes([
@@ -269,8 +269,8 @@ export const DesignBrowser = <T extends Design = Design>(
                 />
               </Section>
             </Stack.Item>
-            <Stack.Item grow>
-              <Section fill scrollable>
+            <Stack.Item grow style={{ overflowY: 'auto', overflowX: 'hidden' }}>
+              <Section fill>
                 {searchText.length > 0 ? (
                   <VirtualList>
                     {sortBy(
