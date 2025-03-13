85f764e7b1fa0b113bc243e417ca73f93cedd03b
ArrisFairburne <arrisfairburne@gmail.com>
2025-03-12 22:53:03 -0400

    Actually I think I'll just use the upstreams' version of the fix

--- tgui/packages/tgui-panel/chat/renderer.jsx
+++ tgui/packages/tgui-panel/chat/renderer.jsx
@@ -623,16 +623,13 @@ class ChatRenderer {
       '</body>\n' +
       '</html>\n';
     // Create and send a nice blob
-    const blob = new Blob([pageHtml]);
+    const blob = new Blob([pageHtml], { type: 'text/plain' });
     const timestamp = new Date()
       .toISOString()
       .substring(0, 19)
       .replace(/[-:]/g, '')
       .replace('T', '-');
-    const a = document.createElement('a');
-    a.download = `ss13-chatlog-${timestamp}.html`;
-    a.href = URL.createObjectURL(blob);
-    a.click();
+    Byond.saveBlob(blob, `ss13-chatlog-${timestamp}.html`, '.html');
   }
 }
 
