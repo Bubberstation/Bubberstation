a740bc4d06587fb04d88ec4cf5ba7577306a6b82
Vanilla1040 <Tim.vanilla1010@yahoo.com>
2025-04-08 21:33:26 +0200

    Fixed Comments

--- tgui/packages/tgui/interfaces/StripMenu.tsx
+++ tgui/packages/tgui/interfaces/StripMenu.tsx
@@ -78,11 +78,11 @@ const ALTERNATE_ACTIONS: Record<string, AlternateAction> = {
     icon: 'microchip',
     text: 'Adjust sensors',
   },
-  // NOVA ADDITION BEGIN - entombed quirk suit reactivation
+  // BUBBER EDIT BEGIN - entombed quirk suit reactivation
   entombed_emergency_reactivate: {
     icon: 'power-off',
     text: 'Emergency MODsuit reactivation',
-  }, // NOVA ADDITION END
+  }, // BUBBER EDIT END
 };
 
 const SLOTS: Record<
