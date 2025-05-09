557bc53c092b98a3f73f33efb498d364c431cd3d
Roxy <75404941+TealSeer@users.noreply.github.com>
2025-05-08 20:00:42 -0400

    Fixes for #90905

--- tools/ci/copy_build_output.sh
+++ tools/ci/copy_build_output.sh
@@ -7,3 +7,7 @@ mkdir -p \
 cp tgstation.dmb tgstation.rsc $1/
 cp -r icons/* $1/icons/
 cp -r tgui/public/* $1/tgui/public/
+
+# BUBBER EDIT ADDITION BEGIN - Call our deploy too
+source modular_zubbers/tools/deploy_bubber.sh
+# BUBBER EDIT ADDITION END - Call our deploy too
