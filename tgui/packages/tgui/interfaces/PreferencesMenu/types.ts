164cc66c914bb978bd84e4d75523ad94e5c8b3ec
Roxy <75404941+TealSeer@users.noreply.github.com>
2025-06-01 19:31:00 -0400

    Remove unused is_vetted check This data is never used by the UI and is potentially causing unnecessary DB queries

--- tgui/packages/tgui/interfaces/PreferencesMenu/types.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/types.ts
@@ -181,7 +181,6 @@ export type PreferencesMenuData = {
   preview_options: string[]; // SKYRAT EDIT ADDITION
   preview_selection: string; // SKYRAT EDIT ADDITION
 
-  is_vetted: BooleanLike; // BUBBER EDIT - Vetted Users
   character_preferences: {
     clothing: Record<string, string>;
     features: Record<string, string>;
