2f4efa74c90bb7dc145830f217b3a67ff5c78419
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2025-06-22 00:17:28 -0700

    [tgui] Reworks dev server into Bun + TS (#91695)
    ## About The Pull Request
    This started out as just a small fix for dev server crashing but lo, I
    cannot keep my hands off of such a juicy rework, it now uses Bun's own
    websocket server and is written entirely in typescript. It makes much
    more use of console.log (you can see these in client) as well
    ## Why It's Good For The Game
    Lets devs use tgui-dev again
    
    ## Changelog

--- tgui/packages/tgui/index.tsx
+++ tgui/packages/tgui/index.tsx
@@ -11,7 +11,7 @@ import './styles/themes/clockwork.scss'; // SKYRAT EDIT ADDITION
 import { perf } from 'common/perf';
 import { setupGlobalEvents } from 'tgui-core/events';
 import { setupHotKeys } from 'tgui-core/hotkeys';
-import { setupHotReloading } from 'tgui-dev-server/link/client.mjs';
+import { setupHotReloading } from 'tgui-dev-server/link/client';
 
 import { App } from './App';
 import { setGlobalStore } from './backend';
