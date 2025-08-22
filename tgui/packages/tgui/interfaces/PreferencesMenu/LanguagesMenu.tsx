30c2969750aea86a9b3f1249ed525e56db4677c7
ReturnToZender <110273561+ReturnToZender@users.noreply.github.com>
2025-08-22 04:04:26 -0500

    New LanguagesMenu.tsx update

--- tgui/packages/tgui/interfaces/PreferencesMenu/LanguagesMenu.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/LanguagesMenu.tsx
@@ -1,5 +1,5 @@
 // THIS IS A SKYRAT UI FILE
-import { Box, Button, Section, Stack } from 'tgui-core/components';
+import { Box, Button, Section, Stack, Tooltip } from 'tgui-core/components';
 
 import { useBackend } from '../../backend';
 import type { PreferencesMenuData } from './types';
@@ -30,21 +30,29 @@ export const KnownLanguage = (props) => {
   );
 };
 
-export const UnknownLanguage = (props) => {
+export const UnknownLanguage = ({ language, isAtLimit, remaining }) => {
   const { act } = useBackend<PreferencesMenuData>();
+  const tooltipContent = isAtLimit
+    ? "You've reached the maximum number of languages."
+    : `You have ${remaining} language${remaining === 1 ? '' : 's'} remaining.`;
+
   return (
     <Stack.Item>
-      <Section title={props.language.name}>
-        {props.language.description}
+      <Section title={language.name}>
+        {language.description}
         <br />
-        <Button
-          color="good"
-          onClick={() =>
-            act('give_language', { language_name: props.language.name })
-          }
-        >
-          Learn <Box className={`languages16x16 ${props.language.icon}`} />
-        </Button>
+        <Tooltip content={tooltipContent}>
+          <Button
+            color="good"
+            disabled={isAtLimit}
+            onClick={() =>
+              !isAtLimit &&
+              act('give_language', { language_name: language.name })
+            }
+          >
+            Learn <Box className={`languages16x16 ${language.icon}`} />
+          </Button>
+        </Tooltip>
       </Section>
     </Stack.Item>
   );
@@ -52,32 +60,36 @@ export const UnknownLanguage = (props) => {
 
 export const LanguagesPage = (props) => {
   const { data } = useBackend<PreferencesMenuData>();
+
+  const currentCount = data.selected_languages.length;
+  const maxAllowed = data.total_language_points;
+  const isAtLimit = currentCount >= maxAllowed;
+  const remaining = Math.max(maxAllowed - currentCount, 0);
+
   return (
     <Stack>
-      <Stack.Item minWidth="33%">
-        <Section title="Available Languages">
+      <Stack.Item minWidth="50%" style={{ marginRight: '6px' }}>
+        <Section title={`Available Languages (${remaining} remaining)`}>
           <Stack vertical>
             {data.unselected_languages.map((val) => (
-              <UnknownLanguage key={val.icon} language={val} />
+              <UnknownLanguage
+                key={val.icon}
+                language={val}
+                isAtLimit={isAtLimit}
+                remaining={remaining}
+              />
             ))}
           </Stack>
         </Section>
       </Stack.Item>
-      <Stack.Item minWidth="33%">
-        <Section
-          title={
-            'Points: ' +
-            data.selected_languages.length +
-            '/' +
-            data.total_language_points
-          }
-        >
-          Here, you can purchase languages using a point buy system. Each
-          Language is worth 1 point.
-        </Section>
-      </Stack.Item>
-      <Stack.Item minWidth="33%">
-        <Section title="Known Languages">
+
+      <Stack.Item minWidth="50%">
+        <Section title={`Known Languages (${currentCount} of ${maxAllowed})`}>
+          {isAtLimit && (
+            <Box color="bad" mb={1}>
+              You have reached the maximum number of languages.
+            </Box>
+          )}
           <Stack vertical>
             {data.selected_languages.map((val) => (
               <KnownLanguage key={val.icon} language={val} />
