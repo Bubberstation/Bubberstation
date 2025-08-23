9cc25ae4e391ac4fd8ec7974c4ef2ca34c3a8276
goat <100518708+sheepishgoat@users.noreply.github.com>
2025-08-23 01:33:31 -0400

    this should help?

---
+++ modular_gs/tools/ci/deploy_gs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script will deploy our modular icon assets so iconforge can use them in spritesheet generation
+# This includes modular_gs/icons and greyscale json configs
+
+mkdir -p \
+    $1/modular_gs/icons \
+    $1/modular_gs/code/datums/greyscale/json_configs
+
+cp -r modular_gs/icons/* $1/modular_gs/icons/
+cp -r modular_gs/code/datums/greyscale/json_configs/* $1/modular_gs/code/datums/greyscale/json_configs/
