7e013e0d40ad873f713ecc236609a3cef692d0b3
ReturnToZender <110273561+ReturnToZender@users.noreply.github.com>
2025-09-08 19:34:06 -0500

    Hack fix

--- tgui/packages/tgui/interfaces/ExaminePanel.jsx
+++ tgui/packages/tgui/interfaces/ExaminePanel.jsx
@@ -46,6 +46,7 @@ export const ExaminePanel = () => {
   const [lowerTabIndex, setLowerTabIndex] = useState(1);
   const [page, setPage] = useState('main');
   const { act, data } = useBackend();
+
   const {
     character_name,
     obscured,
@@ -65,6 +66,13 @@ export const ExaminePanel = () => {
   const handlePageChange = (page, newPage) => {
     setPage(newPage);
   };
+
+  const [previewKey, setPreviewKey] = useState(0);
+  if (previewKey === 0 && assigned_map) {
+    setTimeout(() => {
+      setPreviewKey(1);
+    }, 200);
+  }
 
   return (
     <Window
@@ -100,6 +108,7 @@ export const ExaminePanel = () => {
                     title="Character Preview"
                   >
                     <ByondUi
+                      key={previewKey}
                       height="100%"
                       width="100%"
                       className="ExaminePanel__map"
@@ -117,6 +126,7 @@ export const ExaminePanel = () => {
                       style={{ textAlign: 'center' }}
                     >
                       <ByondUi
+                        key={previewKey}
                         height="260px"
                         width="100%"
                         className="ExaminePanel__map"
