Bubberstation/Bubberstation

commit b731a16ab39be891db9305a7e85d89754d0d6e95

only load vetted status as needed and once per client (#3594)
## About The Pull Request

deletes some global lists and stuff that definately could just be loaded
as needed
## Why It's Good For The Game

free up some MEMORY. load it FASTER. win-win

## Proof Of Testing
<details>
<summary>Screenshots/Videos</summary>

</details>

## Changelog
:cl:
refactor: vetted system now hotloads ranks
/:cl:

modular_skyrat/modules/player_ranks/code/subsystem/player_ranks.dm
@@ -29,7 +29,6 @@ SUBSYSTEM_DEF(player_ranks)
 
 	load_donators()
 	load_mentors()
-	load_vetted_ckeys()
 	return SS_INIT_SUCCESS
 
 

modular_zubbers/code/modules/vetted/vetted.dm
@@ -2,60 +2,39 @@ GLOBAL_LIST_EMPTY(vetted_list_legacy)
 GLOBAL_PROTECT(vetted_list_legacy)
 GLOBAL_LIST_EMPTY(vetted_list)
 GLOBAL_PROTECT(vetted_list)
-/datum/controller/subsystem/player_ranks
-	var/loaded_vetted_sql = FALSE
 
 /datum/player_rank_controller/vetted
 	rank_title = "vetted user"
 	var/file_path_vetted
 
+/client/
+	var/is_vetted = null
+
 /datum/controller/subsystem/player_ranks/proc/is_vetted(client/user, admin_bypass = TRUE)
 	if(!istype(user))
 		CRASH("Invalid user type provided to is_vetted(), expected 'client' and obtained '[user ? user.type : "null"]'.")
-
-	if(!CONFIG_GET(flag/check_vetted))
-		return TRUE
-
-	if(GLOB.vetted_list[user.ckey])
-		return TRUE
-
-	if(admin_bypass && is_admin(user))
-		return TRUE
-
-	return FALSE
+	if(!isnull(user.is_vetted))
+		return user.is_vetted
+	if(get_user_vetted_status_hot(user.ckey))
+		user.is_vetted = TRUE
+		return user.is_vetted
+	else
+		user.is_vetted = FALSE
+		return user.is_vetted
 
 
-/datum/controller/subsystem/player_ranks/proc/load_vetted_ckeys()
-	PROTECTED_PROC(TRUE)
-	if(loaded_vetted_sql)
-		return
+
+/datum/controller/subsystem/player_ranks/proc/get_user_vetted_status_hot(ckey)
 	if(IsAdminAdvancedProcCall())
 		return
-
-	vetted_controller = new
-	vetted_controller.file_path_vetted = "[global.config.directory]/bubbers/vetted_players.txt"
-	ASYNC
-		for(var/line in world.file2list(vetted_controller.file_path_vetted))
-			if(!line)
-				continue
-
-			if(findtextEx(line, "#", 1, 2))
-				continue
-
-			vetted_controller.add_player(line, legacy = TRUE)
-			world.log << "Added [line] to vetted list."
-		var/datum/db_query/query_load_player_rank = SSdbcore.NewQuery("SELECT * FROM vetted_list")
-		if(!query_load_player_rank.warn_execute())
-			qdel(query_load_player_rank)
-			return
-		while(query_load_player_rank.NextRow())
-			var/ckey = ckey(query_load_player_rank.item[1])
-			vetted_controller.add_player(ckey)
-			world.log << "Added [ckey] to vetted list."
+	var/datum/db_query/query_load_player_rank = SSdbcore.NewQuery("SELECT ckey FROM vetted_list WHERE ckey = :ckey", list("ckey" = ckey))
+	if(!query_load_player_rank.warn_execute())
 		qdel(query_load_player_rank)
-
-	loaded_vetted_sql = TRUE
-	return TRUE
+		return
+	while(query_load_player_rank.NextRow())
+		var/ckey2 = ckey(query_load_player_rank.item[1])
+		. = ckey2
+	qdel(query_load_player_rank)
 
 /datum/player_rank_controller/vetted/proc/convert_all_to_sql()
 	if(!SSdbcore.Connect())
@@ -82,7 +61,8 @@ GLOBAL_PROTECT(vetted_list)
 	if(legacy)
 		GLOB.vetted_list_legacy[ckey] = TRUE
 	GLOB.vetted_list[ckey] = TRUE
-	add_player_to_sql(ckey, admin)
+	if(admin)
+		add_player_to_sql(ckey, admin)
 
 /datum/player_rank_controller/vetted/remove_player(ckey)
 	GLOB.vetted_list -= ckey
@@ -101,6 +81,7 @@ ADMIN_VERB(convert_flatfile_vettedlist_to_sql, R_DEBUG, "Convert Vetted list to 
 	if(consent == "Yes")
 		SSplayer_ranks.vetted_controller.convert_all_to_sql()
 		message_admins("[usr] has forcefully converted the vetted list file to SQL.")
+
 ADMIN_VERB(add_vetted, R_ADMIN, "Add user to Vetted", "Adds a user to the vetted list", ADMIN_CATEGORY_MAIN)
 	var/user_adding = tgui_input_text(usr, "Whom is being added?", "Vetted List")
 	if(length(user_adding))

modular_zubbers/code/modules/vetted/overrides/erp_preferences.dm
@@ -1,13 +1,16 @@
 /datum/preference/toggle/master_erp_preferences/is_accessible(datum/preferences/preferences)
 	. = ..()
-	if(.)
-		if(!SSplayer_ranks.is_vetted(preferences.parent, admin_bypass = FALSE))
-			return FALSE
-	return .
+	if(preferences.parent.is_vetted)
+		return TRUE
+	ASYNC
+		SSplayer_ranks.is_vetted(preferences.parent, admin_bypass = FALSE)
+
 
 /datum/preference/toggle/erp/is_accessible(datum/preferences/preferences)
 	. = ..()
-	if(.)
-		if(!SSplayer_ranks.is_vetted(preferences.parent, admin_bypass = FALSE))
-			return FALSE
-	return .
+	if(preferences.parent.is_vetted)
+		return TRUE
+
+	ASYNC
+		SSplayer_ranks.is_vetted(preferences.parent, admin_bypass = FALSE)
+

commit 9e1f867493a83d0c3a0b53a6f15544623e2d7369

fix vetted command (#4781)
## About The Pull Request
fix vetted command, ozzo broke it in
https://github.com/Bubberstation/Bubberstation/commit/b731a16ab39be891db9305a7e85d89754d0d6e95

## Why It's Good For The Game
admins need it apparently

## Proof Of Testing
tested this on a local DB

## Changelog

:cl:
admin: vetted command works again
/:cl:

modular_skyrat/modules/player_ranks/code/subsystem/player_ranks.dm
@@ -29,6 +29,7 @@ SUBSYSTEM_DEF(player_ranks)
 
 	load_donators()
 	load_mentors()
+	vetted_controller = new()
 	return SS_INIT_SUCCESS
 
 

modular_zubbers/code/modules/vetted/vetted.dm
@@ -5,7 +5,6 @@ GLOBAL_PROTECT(vetted_list)
 
 /datum/player_rank_controller/vetted
 	rank_title = "vetted user"
-	var/file_path_vetted
 
 /client/
 	var/is_vetted = null