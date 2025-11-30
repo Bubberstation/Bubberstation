d3668036f2deae065f2fdc18aa6c35ce645c650e
shayoki <96078776+shayoki@users.noreply.github.com>
2025-11-30 04:05:44 -0600

    spaceruin noops and spaceheater death
    my first commit as a maptainer i sure hope this doesn't make things worse

--- tools/mapping_fix_template_noop_areas/launch_fix_template_noop_in_maps.bat
+++ tools/mapping_fix_template_noop_areas/launch_fix_template_noop_in_maps.bat
@@ -5,5 +5,7 @@ set PY_SCRIPT=fix_template_noop_in_maps.py
 
 REM Example: search the SpaceRuins folder relative to git root
 python "%PY_SCRIPT%" "_maps\RandomRuins\SpaceRuins"
-
+REM BUBBER EDIT: Below are our custom spaceruins. See: tools\maplint\source\lint.py for specific maps to skip
+python "%PY_SCRIPT%" "__maps\RandomRuins\SpaceRuins\skyrat"
+python "%PY_SCRIPT%" "__maps\RandomRuins\SpaceRuins\bubberstation"
 endlocal
