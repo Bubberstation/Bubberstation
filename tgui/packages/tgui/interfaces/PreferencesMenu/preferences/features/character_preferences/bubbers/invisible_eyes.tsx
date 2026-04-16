68d661f3c4e984a73ba19dadb095e25b6d16730b
MrGloopy <155857749+MrGloopy@users.noreply.github.com>
2026-04-16 20:39:51 +1000

    added initial files i think i dont know

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/bubbers/invisible_eyes.tsx
@@ -0,0 +1,7 @@
+import { type Feature, CheckboxInput, FeatureToggle } from '../../base';
+export const invisible_eyes_toggle: Feature<string>= {
+    name: 'Invisible Eyes',
+    description: 'Whether your eyes are invisible or not',
+    component: FeatureCheckboxinput,
+
+};
